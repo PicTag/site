40c15a9663f1730537505cb86589de8687fe5d82
YuanSamuel <samuel6161@gmail.com>
2022-04-12 11:26:39 -0500

    :sparkles: about updates

--- pages/index.js
+++ pages/index.js
@@ -12,23 +12,23 @@ export default function Home() {
       </Head>
 
       <main className='flex flex-col items-center justify-center w-screen h-screen'>
-        <div className='flex flex-col justify-center items-center'>
-          <p className=' text-4xl text-blue-500 font-bold'>Welcome to PicTag</p>
-          <div className='flex flex-row w-full justify-around'>
+        <div className='flex flex-col justify-center items-center w-full h-full'>
+          <p className=' text-4xl text-blue-500 font-bold pb-10'>Welcome to PicTag</p>
+          <div className='flex flex-row w-full justify-around px-20'>
             <div className='flex flex-col items-center'>
-              <Image src='/samuel.jpg' alt='Samuel' width={300} height={300} />
-              <p>Samuel</p>
-              <p>samuel@pictag.us</p>
+              <Image src='/samuel1.png' alt='Samuel Yuan' width={300} height={300} />
+              <p>Samuel Yuan</p>
+              <a href='mailto:samuel@pictag.us' target='_blank' rel='noreferrer' className='text-blue-500'>samuel@pictag.us</a>
             </div>
             <div className='flex flex-col items-center'>
               <Image src='/sarv.png' alt='Sarvesh' width={300} height={300} />
-              <p>Sarvesh</p>
-              <p>sarvesh@pictag.us</p>
+              <p>Sarvesh Submarineian</p>
+              <a href='mailto:sarvesh@pictag.us' target='_blank' rel='noreferrer' className='text-blue-500'>sarvesh@pictag.us</a>
             </div>
             <div className='flex flex-col items-center'>
-              <Image src='/tyla.jpeg' alt='Tyler' width={300} height={300} />
-              <p>Tyler</p>
-              <p>tyler@pictag.us</p>
+              <Image src='/tyla1.png' alt='Tyler' width={300} height={300} />
+              <p>Tyler  Kerch</p>
+              <a href='mailto:tyler@pictag.us' target='_blank' rel='noreferrer' className='text-blue-500'>tyler@pictag.us</a>
             </div>
             
             
