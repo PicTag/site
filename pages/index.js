2c77765d32fc8cd8caf0a60f04060ec2fbdc0e09
YuanSamuel <samuel6161@gmail.com>
2022-04-12 00:58:06 -0500

    update

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="pictag.us">PicTag!</a>
+          Welcome to <a href="/">PicTag!</a>
         </h1>
 
         <p className={styles.description}>
